15b36536a356242b195160ab0d1725937e1b661b
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-07 17:53:05 +0530

    [roshan]creat contact

--- adressBook/Program.cs
+++ adressBook/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welecome to adress book ");
+           // Console.WriteLine("welecome to adress book ");
+            AddressBook addressBook = new AddressBook();
+
+            addressBook.AddContact("Roshan", "Palkandwar", "Age-25 ", "Wardha", "Maharashtra", 442001, 9823760189, "roshanPalkandwar@gmail.com");
+            
+            
+             addressBook.AddContact("prasad", "patare", "Age-23", "amrawati", "maharashtra", 453326, 9845783465, "prasadpatare@gmail.com");
+            
+            addressBook.ViewContact();
+            
+
         }
     }
 }
