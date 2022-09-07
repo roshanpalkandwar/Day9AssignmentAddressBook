c8a76bf4bbb5b6000fb010f61e3c2822067fe670
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-07 16:09:00 +0530

    [roshan]welecome masage

---
+++ adressBook/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace adressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welecome to adress book ");
+        }
+    }
+}
